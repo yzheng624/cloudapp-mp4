d7a3bdb15c1300b54bf484501ea1db0366207f3e
Sayed Hadi Hashemi <public@xldrx.com>
2015-09-12 17:12:30 -0500

    Minor Fixes

--- internal_use/submit.py
+++ internal_use/submit.py
@@ -21,7 +21,7 @@ class MP4(CourseraSubmission):
         self.part_names = ["Create and Populate an HBase Table"]
         PREFIX = os.environ["PREFIX"]
 
-        self.files_results = ["SuperTable.output"]
+        self.files_results = [os.path.join(PREFIX, "SuperTable.output")]
         self.files_codes = [
             [os.path.join(PREFIX, "SuperTable.java"),
              os.path.join(PREFIX, "SuperTable.log"),
